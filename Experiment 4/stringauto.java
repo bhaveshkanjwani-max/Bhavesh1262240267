290a695a7d7b7ed5559021bacd18f5a4e0a000b4
bhaveshkanjwani-max <bhaveshkanjwani@gmail.com>
2026-09-22 21:54:11 +0530

    stringauto.java

---
+++ Experiment 4/stringauto.java
@@ -0,0 +1,10 @@
+public class stringauto
+{
+    public static void main(String args[])
+    {
+        String  s="xyz";
+        char t=s.charAt(2);
+        System.out.println("Wrapper String is : "+s);
+        System.out.println("character is : "+t);
+    }
+}
